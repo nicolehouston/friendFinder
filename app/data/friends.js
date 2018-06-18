011ae65b2ee9bc0fc214c6d9b3c2ecae717a094a
Nicole Houston <nicole.houston@cox.net>
2018-06-18 09:17:52 -0700

    wrote function to calculate compatability score

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,5 +1,20 @@
 var friendArray = [
-
+    {
+        name: "Doug Funny",
+        picture: "https://vignette.wikia.nocookie.net/doug/images/a/a5/Doug_Funnie2.png/revision/latest?cb=20180531191407",
+        scores: [
+            3,
+            5,
+            2, 
+            1,
+            5,
+            4,
+            4,
+            4,
+            2,
+            5
+        ]
+    }
 ];
 
 module.exports = friendArray;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,5 +1,18 @@
 var friendData = require("../data/friends.js");
 
+function calculateScore(arr1, arr2) {
+    var score = 0;
+    var scoreArray = [];
+    for(var i = 0; i < arr1.length; i++) {
+        scoreArray.push(Math.abs(parseInt(arr1[i]) - parseInt(arr2[i])));
+    };
+    for(var j= 0; j < scoreArray.length; j++) {
+        score += scoreArray[j];
+    };
+    console.log(score);
+    return score;
+};
+
 module.exports = function(app) {
 
     app.get("/api/friends", function(req, res) {
@@ -7,11 +20,21 @@ module.exports = function(app) {
     });
 
     app.post("/api/friends", function(req, res) {
-        friendData.push(req.body);
         newScores = req.body.scores;
 
-        console.log(newScores);
-        
+        var mostCompatibleScore = 50;
+        var mostCompatibleFriend = {};
+
+        for(var i = 0; i < friendData.length; i++) {
+            var result = calculateScore(newScores, friendData[i].scores);
+            if(result < mostCompatibleScore) {
+                mostCompatibleScore = result;
+                mostCompatibleFriend = friendData[i];
+            }
+        }
+        console.log(mostCompatibleScore);
+        console.log(mostCompatibleFriend);
+        friendData.push(req.body);
         // var leastDifference = 0;
         // var compScore = 0;
         // var compScoreArr = [];
