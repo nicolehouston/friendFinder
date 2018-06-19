ac3dc1446d0376ccd9d5af43fe999fd3c8525f13
Nicole Houston <nicole.houston@cox.net>
2018-06-18 20:40:22 -0700

    added one more friend to data

--- app/data/friends.js
+++ app/data/friends.js
@@ -5,13 +5,29 @@ var friendArray = [
         scores: [
             3,
             5,
-            2, 
+            5, 
+            3,
+            2,
+            3,
             1,
+            2,
+            4,
+            5
+        ]
+    },
+    {
+        name: "Patty Mayonnaise",
+        picture: "https://vignette.wikia.nocookie.net/nickelodeon/images/1/1e/Patti_Mayonnaise.png/revision/latest?cb=20161009032853",
+        scores: [
             5,
             4,
+            5,
+            1,
+            5,
+            3,
             4,
             4,
-            2,
+            3,
             5
         ]
     }
